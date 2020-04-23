a60c9af50de4a58db6c5a9ecbe06669d31ce609c
IveriNarozashvili <geobmw91>
2020-04-23 13:46:52 +0300

    Update code

---
+++ src/main/java/Ivo/probnoe/ProbnoeApplication.java
@@ -0,0 +1,14 @@
+package Ivo.probnoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbnoeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProbnoeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/Ivo/probnoe/controller/MessageController.java
@@ -0,0 +1,63 @@
+package Ivo.probnoe.controller;
+
+
+import Ivo.probnoe.exceptions.NotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+@RestController
+@RequestMapping("message")
+public class MessageController {
+    private int counter = 4;
+
+    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>() {{ put("id", "1"); put("Текст", "Один"); }});
+        add(new HashMap<String, String>() {{ put("id", "2"); put("Текст", "Два"); }});
+        add(new HashMap<String, String>() {{ put("id", "3"); put("Текст", "Три"); }});
+    }};
+
+    @GetMapping
+    public List<Map<String, String>> list() {
+        return messages;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getMessage(id);
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
+        return messages.stream()
+                .filter(message -> message.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> message) {
+        message.put("id", String.valueOf(counter++));
+
+        messages.add(message);
+
+        return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
+        Map<String, String> messageFromDb = getMessage(id);
+
+        messageFromDb.putAll(message);
+        messageFromDb.put("id", id);
+
+        return messageFromDb;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> message = getMessage(id);
+
+        messages.remove(message);
+    }}
